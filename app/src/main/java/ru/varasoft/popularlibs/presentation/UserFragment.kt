8958df330ee92e2493ef45e611cb349b9a15065f
Vadim <vraspopin@inbox.ru>
2021-06-16 11:31:24 +0300

    Исправление ошибок

--- app/src/main/java/ru/varasoft/popularlibs/presentation/UserFragment.kt
+++ app/src/main/java/ru/varasoft/popularlibs/presentation/UserFragment.kt
@@ -9,27 +9,29 @@ import moxy.ktx.moxyPresenter
 import ru.varasoft.popularlibs.App
 import ru.varasoft.popularlibs.BackButtonListener
 import ru.varasoft.popularlibs.arguments
+import ru.varasoft.popularlibs.data.user.GithubUserRepository
 import ru.varasoft.popularlibs.data.user.UserRepositoryFactory
+import ru.varasoft.popularlibs.data.user.model.GithubUser
 import ru.varasoft.popularlibs.databinding.FragmentUserBinding
 
 class UserFragment : MvpAppCompatFragment(), UserView, BackButtonListener {
     companion object {
 
-        private const val ARG_USER_ID = "userId"
+        private const val ARG_USER_ID = "GithubUser"
 
-        fun newInstance(userId: String): Fragment =
+        fun newInstance(userId: GithubUser): Fragment =
             UserFragment()
                 .arguments(ARG_USER_ID to userId)
     }
 
-    private val userId: String by lazy {
-        arguments?.getString(ARG_USER_ID) ?: ""
+    private val user: GithubUser? by lazy {
+        arguments?.getParcelable(ARG_USER_ID)
     }
     val presenter: UserPresenter by moxyPresenter {
         UserPresenter(
-            userId,
+            user!!,
             App.instance.router,
-            userRepository = UserRepositoryFactory.create()
+            userRepository = GithubUserRepository()
         )
     }
 
--- app/src/main/java/ru/varasoft/popularlibs/presentation/UserPresenter.kt
+++ app/src/main/java/ru/varasoft/popularlibs/presentation/UserPresenter.kt
@@ -3,13 +3,13 @@ package ru.varasoft.popularlibs.presentation
 import com.github.terrakok.cicerone.Router
 import io.reactivex.disposables.Disposable
 import moxy.MvpPresenter
-import ru.varasoft.popularlibs.data.user.UserRepository
 import ru.varasoft.popularlibs.data.user.model.GithubUser
+import ru.varasoft.popularlibs.data.user.model.IGithubUsersRepo
 
 class UserPresenter(
-    private val userId: String,
+    private val user: GithubUser,
     private val router: Router,
-    private val userRepository: UserRepository
+    private val userRepository: IGithubUsersRepo
 ) : MvpPresenter<UserView>() {
 
     lateinit var userDisposable: Disposable
@@ -17,7 +17,7 @@ class UserPresenter(
     override fun onFirstViewAttach() {
         super.onFirstViewAttach()
         var user: GithubUser? = null
-        userDisposable = userRepository.fetchUserById(userId)
+        userDisposable = userRepository.getUsers(this.user)
             .subscribe({ s ->
                 user = s
             },
